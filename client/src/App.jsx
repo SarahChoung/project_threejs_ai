acdda29a8f0464dc136153c257758717029d1dd2
Sarah Choung <sarahjchoung@gmail.com>
2025-05-13 23:18:47 -0700

    get it to compile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import Canvas from './canvas';
-import Customizer from './pages/Customizer';
-import Home from './pages/Home';
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -8,8 +8,11 @@ function App() {
       <Home />
       <Canvas />
       <Customizer />
+      <div>
+        <h1 className="head-text">ThreeJS</h1>
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
